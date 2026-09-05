bf0888928f8df67b3c5a85fb2c96bcdcd8b83aca
isegzidrafok <capo17926@gmail.com>
2026-09-05 22:34:04 +0300

    Fix MainActivity class declaration syntax

--- app/src/main/java/com/example/miniapp/MainActivity.kt
+++ app/src/main/java/com/example/miniapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.miniapp
 import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : Activity {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
